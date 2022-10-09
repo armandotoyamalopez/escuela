55271b3c4a63b73ab55a20ef7f82d693ba98c97d
ArmandoToyama <114810347+ArmandoToyama@users.noreply.github.com>
2022-10-08 21:31:18 -0700

    instalando primer aplicativo

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,26 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
 
-export default NextAuth({
+export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
+  secret: process.env.AUTH_SECRET,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
 
   pages: {
     signIn: "/auth/signin",
   },
 
+
+  
   callbacks: {
     async session({ session, token }) {
       session.user.username = session.user.name
@@ -25,4 +31,13 @@ export default NextAuth({
       return session;
     },
   },
-});
+
+
+
+
+}
+
+
+export default NextAuth(authOptions)
+
+ 
